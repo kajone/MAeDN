e6bf27848a49efadff0f0d752c9bfb4601a11636
Jannis <j.konopka@fz-juelich.de>
2015-12-01 10:19:03 +0100

    GameTokens Konstruktor und toString hinzugefuegt

--- src/logik/GameTokens.java
+++ src/logik/GameTokens.java
@@ -2,8 +2,40 @@ package logik;
 
 public class GameTokens {
 
-	private Token[] tokenArray;
+	private Token[] tokenArray = new Token[16];
 	
+	
+	public GameTokens(Player[] player){
+		
+		// 16 Spielsteine erstellen und im tokenArray abspeichern
+		int tokenCounter = 0;
+		for(int i = 1; i < 5; i++){
+			for(int j = 1; j < 5; j++){
+				tokenArray[tokenCounter] = new Token((i)*10+j, player[i-1].getColor());
+				tokenCounter++;
+			}	
+		}	
+	}
+	
+	// Ausgabe der 16 Spielsteine
+	public String toString(){
+		
+		String s = "";
+		
+		for(int i = 0; i < tokenArray.length; i++){
+			s += "Spielstein: " + tokenArray[i].getId() + " mit der Farbe " +  
+				  tokenArray[i].getColor() + " auf Position " + tokenArray[i].getPosition() + "\n";
+		}
+		
+		return s;
+		
+	}
+	
+//	public static void main(String[] args){
+//		
+//		
+//		
+//	}
 	
 	
 }
--- src/logik/MainClass.java
+++ src/logik/MainClass.java
@@ -12,12 +12,17 @@ public class MainClass {
 		Player[] player = {new RealPlayer("Hans", "red",1), new RealPlayer("Kajo", "green",2),
 						   new BotPlayer("Bot1", "black",3), new BotPlayer("Bot2","violett",4)};
 		
+		
+		
+		GameTokens gameTokens = new GameTokens(player);
+		System.out.println(gameTokens.toString());
+		
 //		File f = new File("file.txt");
-		GameBoard mainBoard = new GameBoard(player);
+//		GameBoard mainBoard = new GameBoard(player);
 		
 		//mainBoard.play();
-		mainBoard.mergeBoards();
-		System.out.println(mainBoard.printBoard());
+//		mainBoard.mergeBoards();
+//		System.out.println(mainBoard.printBoard());
 	}
 
 }
